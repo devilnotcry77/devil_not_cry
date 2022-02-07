f75dd79ea06aa236ad8398dc6e14c314fb3e69bf
Сергей Городецкий <78065813+devilnotcry77@users.noreply.github.com>
2022-02-08 02:16:13 +0300

    Add files via upload

---
+++ авторизация.py
@@ -0,0 +1,25 @@
+import config
+import os
+import time
+
+password = (CONFIG.LOGIN)
+login = (CONFIG.LOGIN)
+
+a = input('');
+b = input('');
+
+if(a == login && b == password):
+	print('Вход выполнен!')
+
+os.startfile('')
+os.startfile('')
+
+time.sleep(5)
+
+print('Загрузка библиотек')
+
+time.sleep(3)
+
+os.startfile('')
+
+
